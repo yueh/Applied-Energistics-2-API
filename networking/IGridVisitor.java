e9746e23dbec64a9b13b84ddcd5b0b354366ee04
AlgorithmX2 <AlgorithmX2@gmail.com>
2013-11-26 23:29:05 -0600

    -Internal Classes

--- networking/IGridVisitor.java
+++ networking/IGridVisitor.java
@@ -1,6 +1,5 @@
 package appeng.api.networking;
 
-import appeng.me.GridNode;
 
 /**
  * Simple Visitor pattern access to network nodes.
@@ -11,14 +10,13 @@ public interface IGridVisitor
 	/**
 	 * Called for each node on the network.
 	 * 
-	 * By returning false your informing the host to stop visiting nodes beyond
-	 * the current node.
+	 * By returning false your informing the host to stop visiting nodes beyond the current node.
 	 * 
 	 * @param n
 	 *            the starting node.
 	 * 
 	 * @return true to continue visiting nodes beyond this node.
 	 */
-	public boolean visit(GridNode n);
+	public boolean visit(IGridNode n);
 
 }
