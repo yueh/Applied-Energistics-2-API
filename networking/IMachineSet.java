f05f54fd9a4232799fd05d7b4098f795b1ab7c24
AlgorithmX2 <AlgorithmX2@gmail.com>
2013-11-26 23:48:45 -0600

    Some more fun methods or Machine Sets.

--- networking/IMachineSet.java
+++ networking/IMachineSet.java
@@ -3,6 +3,23 @@ package appeng.api.networking;
 public interface IMachineSet extends Iterable<IGridNode>
 {
 
+	/**
+	 * @return the number of machines in in the set.
+	 */
 	int size();
 
+	/**
+	 * @return true if there are no machines in the set
+	 */
+	boolean isEmpty();
+
+	/**
+	 * @return return true if the node is part of the set.
+	 */
+	boolean contains(Object node);
+
+	/**
+	 * @return the machine class for this set.
+	 */
+	Class<? extends IGridHost> getMachineClass();
 }
