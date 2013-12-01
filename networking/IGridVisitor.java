fa5ace5e70cb3f29cc9d4fff93714d8d49372924
AlgorithmX2 <AlgorithmX2@gmail.com>
2013-11-30 18:44:02 -0600

    Added Connection Visitors

--- networking/IGridVisitor.java
+++ networking/IGridVisitor.java
@@ -1,6 +1,5 @@
 package appeng.api.networking;
 
-
 /**
  * Simple Visitor pattern access to network nodes.
  */
@@ -13,10 +12,10 @@ public interface IGridVisitor
 	 * By returning false your informing the host to stop visiting nodes beyond the current node.
 	 * 
 	 * @param n
-	 *            the starting node.
+	 *            the current node.
 	 * 
 	 * @return true to continue visiting nodes beyond this node.
 	 */
-	public boolean visit(IGridNode n);
+	public boolean visitNode(IGridNode n);
 
 }
