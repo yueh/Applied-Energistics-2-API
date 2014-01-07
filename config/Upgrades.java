2c4dfe44d6c295927f70c00d26146c4d54a1db07
AlgorithmX2 <AlgorithmX2@gmail.com>
2014-01-06 21:44:37 -0600

    More Interesting Upgrade Enum

--- config/Upgrades.java
+++ config/Upgrades.java
@@ -1,23 +1,39 @@
 package appeng.api.config;
 
+import java.util.HashMap;
+
+import net.minecraft.item.ItemStack;
+
 public enum Upgrades
 {
 	/**
 	 * Gold Tier Upgrades.
 	 */
-	CAPACITY(0, 2), REDSTONE(0, 1),
+	CAPACITY(0), REDSTONE(0),
 
 	/**
 	 * Diamond Tier Upgrades.
 	 */
-	FUZZY(1, 1), SPEED(1, 4);
+	FUZZY(1), SPEED(1), INVERTER(1);
 
 	public final int myTier;
-	public final int maxInstalled;
+	public final HashMap<ItemStack, Integer> supportedMax = new HashMap<ItemStack, Integer>();
 
-	private Upgrades(int tier, int maxNum) {
+	private Upgrades(int tier) {
 		myTier = tier;
-		maxInstalled = maxNum;
 	}
 
+	/**
+	 * @return list of Items/Blocks that support this upgrade, and how many it supports.
+	 */
+	public HashMap<ItemStack, Integer> getSupported()
+	{
+		return supportedMax;
+	}
+
+	public void registerItem(ItemStack myItem, int maxSupported)
+	{
+		if ( myItem != null )
+			supportedMax.put( myItem, maxSupported );
+	}
 }
