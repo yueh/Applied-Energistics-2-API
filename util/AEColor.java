059f6373edafe5dd12ba3d1dca3e232b323e8477
AlgorithmX2 <AlgorithmX2@gmail.com>
2014-01-08 21:43:27 -0600

    Color Localization

--- util/AEColor.java
+++ util/AEColor.java
@@ -1,5 +1,6 @@
 package appeng.api.util;
 
+import net.minecraft.util.StatCollector;
 
 /**
  * List of all colors supported by AE, their names, and various colors for display.
@@ -9,39 +10,39 @@ package appeng.api.util;
 public enum AEColor
 {
 
-	White("AppEng.Colors.White", 0xBEBEBE, 0xDBDBDB, 0xFAFAFA),
+	White("gui.appliedenergistics2.White", 0xBEBEBE, 0xDBDBDB, 0xFAFAFA),
 
-	Orange("AppEng.Colors.Orange", 0xF99739, 0xFAAE44, 0xF4DEC3),
+	Orange("gui.appliedenergistics2.Orange", 0xF99739, 0xFAAE44, 0xF4DEC3),
 
-	Magenta("AppEng.Colors.Magenta", 0x821E82, 0xB82AB8, 0xC598C8),
+	Magenta("gui.appliedenergistics2.Magenta", 0x821E82, 0xB82AB8, 0xC598C8),
 
-	LightBlue("AppEng.Colors.LightBlue", 0x628DCB, 0x82ACE7, 0xD8F6FF),
+	LightBlue("gui.appliedenergistics2.LightBlue", 0x628DCB, 0x82ACE7, 0xD8F6FF),
 
-	Yellow("AppEng.Colors.Yellow", 0xFFF7AA, 0xF8FF4A, 0xFFFFE8),
+	Yellow("gui.appliedenergistics2.Yellow", 0xFFF7AA, 0xF8FF4A, 0xFFFFE8),
 
-	Lime("AppEng.Colors.Lime", 0x7CFF4A, 0xBBFF51, 0xE7F7D7),
+	Lime("gui.appliedenergistics2.Lime", 0x7CFF4A, 0xBBFF51, 0xE7F7D7),
 
-	Pink("AppEng.Colors.Pink", 0xDC8DB5, 0xF8B5D7, 0xF7DEEB),
+	Pink("gui.appliedenergistics2.Pink", 0xDC8DB5, 0xF8B5D7, 0xF7DEEB),
 
-	Gray("AppEng.Colors.Gray", 0x7C7C7C, 0xA0A0A0, 0xC9C9C9),
+	Gray("gui.appliedenergistics2.Gray", 0x7C7C7C, 0xA0A0A0, 0xC9C9C9),
 
-	LightGray("AppEng.Colors.LightGray", 0x9D9D9D, 0xCDCDCD, 0xEFEFEF),
+	LightGray("gui.appliedenergistics2.LightGray", 0x9D9D9D, 0xCDCDCD, 0xEFEFEF),
 
-	Cyan("AppEng.Colors.Cyan", 0x2F9BA5, 0x51AAC6, 0xAEDDF4),
+	Cyan("gui.appliedenergistics2.Cyan", 0x2F9BA5, 0x51AAC6, 0xAEDDF4),
 
-	Purple("AppEng.Colors.Purple", 0x8230B2, 0xA453CE, 0xC7A3CC),
+	Purple("gui.appliedenergistics2.Purple", 0x8230B2, 0xA453CE, 0xC7A3CC),
 
-	Blue("AppEng.Colors.Blue", 0x2D29A0, 0x514AFF, 0xDDE6FF),
+	Blue("gui.appliedenergistics2.Blue", 0x2D29A0, 0x514AFF, 0xDDE6FF),
 
-	Brown("AppEng.Colors.Brown", 0x724E35, 0xB7967F, 0xE0D2C8),
+	Brown("gui.appliedenergistics2.Brown", 0x724E35, 0xB7967F, 0xE0D2C8),
 
-	Green("AppEng.Colors.Green", 0x45A021, 0x60E32E, 0xE3F2E3),
+	Green("gui.appliedenergistics2.Green", 0x45A021, 0x60E32E, 0xE3F2E3),
 
-	Red("AppEng.Colors.Red", 0xA50029, 0xFF003C, 0xFFE6ED),
+	Red("gui.appliedenergistics2.Red", 0xA50029, 0xFF003C, 0xFFE6ED),
 
-	Black("AppEng.Colors.Black", 0x2B2B2B, 0x565656, 0x848484),
+	Black("gui.appliedenergistics2.Black", 0x2B2B2B, 0x565656, 0x848484),
 
-	Transparent("AppEng.Colors.Transparent", 0x1B2344, 0x895CA8, 0xDABDEF);
+	Transparent("gui.appliedenergistics2.Fluix", 0x1B2344, 0x895CA8, 0xDABDEF);
 
 	/**
 	 * Unlocalized name for color.
@@ -78,6 +79,12 @@ public enum AEColor
 		if ( equals( Transparent ) || color.equals( Transparent ) )
 			return true;
 		return equals( color );
+	}
+
+	@Override
+	public String toString()
+	{
+		return StatCollector.translateToLocal( unlocalizedName );
 	}
 
 }
