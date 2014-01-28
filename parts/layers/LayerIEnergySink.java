d9b2b1442347a580e89c86f1108aeaac2b2b4a82
AlgorithmX2 <AlgorithmX2@gmail.com>
2014-01-28 11:55:35 -0600

    IC2 Layer Stuff.

--- parts/layers/LayerIEnergySink.java
+++ parts/layers/LayerIEnergySink.java
@@ -3,11 +3,62 @@ package appeng.api.parts.layers;
 import ic2.api.energy.tile.IEnergySink;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraftforge.common.ForgeDirection;
+import net.minecraftforge.common.MinecraftForge;
 import appeng.api.parts.IPart;
 import appeng.api.parts.LayerBase;
+import appeng.util.Platform;
 
 public class LayerIEnergySink extends LayerBase implements IEnergySink
 {
+
+	boolean isInIC2 = false;
+
+	final private void addToENet()
+	{
+		// re-add
+		removeFromENet();
+
+		if ( !isInIC2 && Platform.isServer() )
+		{
+			MinecraftForge.EVENT_BUS.post( new ic2.api.energy.event.EnergyTileLoadEvent( this ) );
+			isInIC2 = true;
+		}
+	}
+
+	final private void removeFromENet()
+	{
+		if ( isInIC2 && Platform.isServer() )
+		{
+			MinecraftForge.EVENT_BUS.post( new ic2.api.energy.event.EnergyTileUnloadEvent( this ) );
+			isInIC2 = false;
+		}
+	}
+
+	final private boolean interestedInIC2()
+	{
+		boolean interested = false;
+		for (ForgeDirection dir : ForgeDirection.VALID_DIRECTIONS)
+		{
+			IPart part = getPart( dir );
+			if ( part instanceof IEnergySink )
+			{
+				interested = true;
+				break;
+			}
+		}
+		return interested;
+	}
+
+	@Override
+	public void PartChanged()
+	{
+		super.PartChanged();
+
+		if ( interestedInIC2() )
+			addToENet();
+		else
+			removeFromENet();
+	}
 
 	@Override
 	public boolean acceptsEnergyFrom(TileEntity emitter, ForgeDirection direction)
