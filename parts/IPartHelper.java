4af3e6de0803fcc3074c4c15c7319318e3a3e8aa
AlgorithmX2 <AlgorithmX2@gmail.com>
2014-01-28 11:26:36 -0600

    Removed Links to API Classes in IPartHelper.

--- parts/IPartHelper.java
+++ parts/IPartHelper.java
@@ -1,43 +1,37 @@
 package appeng.api.parts;
 
-import ic2.api.energy.tile.IEnergySink;
-import ic2.api.energy.tile.IEnergySource;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.ISidedInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
-import net.minecraftforge.fluids.IFluidHandler;
-import buildcraft.api.power.IPowerEmitter;
-import buildcraft.api.power.IPowerReceptor;
-import buildcraft.api.transport.IPipeConnection;
 
 public interface IPartHelper
 {
 
 	/**
-	 * Register a new layer with the part layer system, this allows you to write an in between between tile entities and
-	 * parts.
+	 * Register a new layer with the part layer system, this allows you to write
+	 * an in between between tile entities and parts.
 	 * 
 	 * AE By Default includes,
 	 * 
-	 * 1. {@link ISidedInventory} ( and by extension IInventory. )
+	 * 1. ISidedInventory ( and by extension IInventory. )
 	 * 
-	 * 2. {@link IFluidHandler} Forge Fluids
+	 * 2. IFluidHandler Forge Fluids
 	 * 
-	 * 3. {@link IPowerEmitter} BC Power output.
+	 * 3. IPowerEmitter BC Power output.
 	 * 
-	 * 4. {@link IPowerReceptor} BC Power input.
+	 * 4. IPowerReceptor BC Power input.
 	 * 
-	 * 5. {@link IEnergySink} IC2 Power input.
+	 * 5. IEnergySink IC2 Power input.
 	 * 
-	 * 6. {@link IEnergySource} IC2 Power output.
+	 * 6. IEnergySource IC2 Power output.
 	 * 
-	 * 7. {@link IPipeConnection} BC Pipe Connections
+	 * 7. IPipeConnection BC Pipe Connections
 	 * 
-	 * As long as a valid layer is registered for a interface you can simply implement that interface on a part get
-	 * implement it.
+	 * As long as a valid layer is registered for a interface you can simply
+	 * implement that interface on a part get implement it.
 	 * 
-	 * @return true on success, false on failure, usually a error will be logged as well.
+	 * @return true on success, false on failure, usually a error will be logged
+	 *         as well.
 	 */
 	boolean registerNewLayer(String string, Class<?> layerInterface);
 
