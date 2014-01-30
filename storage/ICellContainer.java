e62741c9f3431df1b719d1fd105cfdcd3d04b16f
AlgorithmX2 <AlgorithmX2@gmail.com>
2014-01-30 13:24:10 -0600

    Documentation Update.

--- storage/ICellContainer.java
+++ storage/ICellContainer.java
@@ -5,7 +5,7 @@ import java.util.List;
 import appeng.api.networking.security.IActionHost;
 
 /**
- * Represents a tile that contributes to storage, such as a ME Chest, or ME Drive.
+ * Represents a IGridhost that contributes to storage, such as a ME Chest, or ME Drive.
  */
 public interface ICellContainer extends IActionHost
 {
