86827f1140194d938edf2c376e3673bd2dc48a12
AlgorithmX2 <AlgorithmX2@gmail.com>
2014-06-20 02:05:53 -0500

    Added ICraftingMachine documentation, and added acceptsPlans method.

--- implementations/tiles/ICraftingMachine.java
+++ implementations/tiles/ICraftingMachine.java
@@ -7,6 +7,23 @@ import appeng.api.networking.crafting.ICraftingPatternDetails;
 public interface ICraftingMachine
 {
 
-	boolean pushPattern(ICraftingPatternDetails patternDetails, InventoryCrafting table, ForgeDirection opposite);
+	/**
+	 * inserts a crafting plan, and the necessary items into the crafting machine.
+	 * 
+	 * @param patternDetails
+	 * @param table
+	 * @param ejectionDirection
+	 * 
+	 * @return if it was accepted, all or nothing.
+	 */
+	boolean pushPattern(ICraftingPatternDetails patternDetails, InventoryCrafting table, ForgeDirection ejectionDirection);
+
+	/**
+	 * check if the crafting machine is accepting pushes via pushPattern, if this is false, all cals to push will fail,
+	 * you can try inserting into the inventry instead.
+	 * 
+	 * @return true, if pushPattern can complete, if its false push will always be false.
+	 */
+	boolean acceptsPlans();
 
 }
