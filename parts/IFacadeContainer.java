e2e448a794ad924ac29ad06d48bf7a746bbf38bc
AlgorithmX2 <AlgorithmX2@gmail.com>
2014-08-27 02:44:08 -0500

    Added Needed methods to the IFacadeContainer Interface.

--- parts/IFacadeContainer.java
+++ parts/IFacadeContainer.java
@@ -1,5 +1,10 @@
 package appeng.api.parts;
 
+import io.netty.buffer.ByteBuf;
+
+import java.io.IOException;
+
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.common.util.ForgeDirection;
 
 /**
@@ -26,5 +31,46 @@ public interface IFacadeContainer
 	 * @return the {@link IFacadePart} for a given side, or null.
 	 */
 	IFacadePart getFacade(ForgeDirection s);
+
+	/**
+	 * rotate the facades left.
+	 */
+	void rotateLeft();
+
+	/**
+	 * write nbt data
+	 * 
+	 * @param data
+	 */
+	void writeToNBT(NBTTagCompound data);
+
+	/**
+	 * read from stream
+	 * 
+	 * @param data
+	 * @return
+	 * @throws IOException
+	 */
+	boolean readFromStream(ByteBuf data) throws IOException;
+
+	/**
+	 * read from NBT
+	 * 
+	 * @param data
+	 */
+	void readFromNBT(NBTTagCompound data);
+
+	/**
+	 * write to stream
+	 * 
+	 * @param data
+	 * @throws IOException
+	 */
+	void writeToStream(ByteBuf data) throws IOException;
+
+	/**
+	 * @return true if there are no facades.
+	 */
+	boolean isEmpty();
 
 }
